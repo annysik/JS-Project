607dda5d5cb9a42511911bee9264e85112f7a6a5
Ismail <ismailanimasahunjb@gmail.com>
2025-06-18 22:09:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function exampleCubic(n) {
+     for (var i = 0 ; i < n; i++ ) {
+     console.log(i);
+     for (var j = i; j < i; j++ ) {
+     console.log(j);
+     for (var k = j; j < j; j++ ) {
+     console.log(k);
+     }
+     }
+     }
+ }
+
+ exampleCubic(9)
+
+
+ function exampleLogarithmic(n) {
+     for (var i = 2 ; i <= n; i= i*2 ) {
+     console.log(i);
+     }
+     }
+
+     exampleLogarithmic(10)
